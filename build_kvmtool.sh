954f61d0059a058431fb9f5b92a67da6f264484f
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-01 14:08:38 -0700

    RME (Realm Management Extension) stack

---
+++ build_kvmtool.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+if [ -z "${DISABLE_CONTAINER_CHECK:-}" ]; then
+    ./container.sh ./build_kvmtool.sh
+    exit 0
+fi
+
+clone()
+{
+    ./clone.sh \
+        kvmtool \
+        https://gitlab.arm.com/linux-arm/kvmtool-cca \
+        cca/v7
+}
+
+build()
+{
+    pushd $(readlink -f kvmtool)
+    intercept-build --append \
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
+    sed -i compile_commands.json -e 's/"cc"/"aarch64-linux-gnu-gcc"/'
+    popd
+}
+
+output()
+{
+    mkdir -p out
+    rsync ./kvmtool/lkvm out/
+}
+
+clone
+build
+output
