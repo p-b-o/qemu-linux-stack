ebeb317855c839fabaf274158f5ec1f26c52ee15
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-02-09 21:16:51 -0800

    riscv64: opensbi + linux

--- build_kernel.sh
+++ build_kernel.sh
@@ -23,23 +23,26 @@ build()
 
     pushd $(readlink -f linux)
     rm -f .config
-    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig -j$(nproc)
+    make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- defconfig -j$(nproc)
     # reduce number of timer interrupts
     scripts/config --disable CONFIG_HZ_250
     scripts/config --enable CONFIG_HZ_100
     # nvme
     scripts/config --enable BLK_DEV_NVME
+    # /proc/sysrq-trigger
+    scripts/config --enable CONFIG_MAGIC_SYSRQ
+    # speed up boot by disabling ftrace
+    scripts/config --disable CONFIG_FTRACE
     # iommufd
     # https://docs.kernel.org/driver-api/vfio.html#vfio-device-cdev
     scripts/config --enable IOMMUFD
     scripts/config --enable VFIO_DEVICE_CDEV
-    scripts/config --enable ARM_SMMU_V3_IOMMUFD
 
     # disable all modules
     sed -i -e 's/=m$/=n/' .config
 
-    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- olddefconfig -j$(nproc)
-    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- all -j$(nproc)
+    make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- olddefconfig -j$(nproc)
+    make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- all -j$(nproc)
 
     # compile commands
     ./scripts/clang-tools/gen_compile_commands.py
@@ -48,7 +51,7 @@ build()
         -e 's/-fconserve-stack//' \
         -e 's/-fno-allow-store-data-races//' \
         -e 's/-mabi=lp64//' \
-        -e 's/aarch64-linux-gnu-gcc/clang -target aarch64-pc-none-gnu -Wno-unknown-warning-option -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang/'
+        -e 's/riscv64-linux-gnu-gcc/clang -target riscv64-pc-none-gnu -Wno-unknown-warning-option -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang/'
 
     popd
 
@@ -58,7 +61,7 @@ build()
 output()
 {
     mkdir -p out
-    rsync ./linux/arch/arm64/boot/Image.gz out/
+    rsync ./linux/arch/riscv/boot/Image out/
 }
 
 clone
