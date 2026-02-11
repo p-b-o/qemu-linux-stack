a1ed4363be25beba98d93f2039f390dabb4702a8
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-02-10 17:04:54 -0800

    wip

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if [ $# -lt 1 ]; then
-    echo "usage: qemu_aarch64_cmd"
+    echo "usage: qemu_ppc64_cmd"
     exit 1
 fi
 
@@ -17,12 +17,12 @@ set -x
 -serial mon:stdio \
 -netdev user,id=vnet \
 -device virtio-net-pci,netdev=vnet \
--M virt,secure=on,virtualization=on,gic-version=3,iommu=smmuv3 \
 -cpu max \
+-M pseries \
 -smp 1 \
 -m 8G \
--bios ./out/flash.bin \
--kernel ./out/Image.gz \
+-bios ./out/boot_rom.bin \
+-kernel ./out/zImage \
 -drive format=raw,file=./out/host.ext4,if=virtio \
 -append "nokaslr root=/dev/vda rw init=/init -- $INIT" \
 -virtfs local,path=$(pwd)/,mount_tag=host,security_model=mapped,readonly=off
