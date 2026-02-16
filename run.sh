eb7c3899e70db7ef5d8037182f6dea96dc27a0cd
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-02-15 22:17:36 -0800

    hafnium + optee + secure-smmu
    Flags taken from:
    https://github.com/OP-TEE/build/blob/master/qemu_v8.mk

--- run.sh
+++ run.sh
@@ -17,7 +17,8 @@ set -x
 -serial mon:stdio \
 -netdev user,id=vnet \
 -device virtio-net-pci,netdev=vnet \
--M virt,secure=on,virtualization=on,gic-version=3,iommu=smmuv3 \
+-M virt,secure=on,virtualization=on,gic-version=3,iommu=smmuv3,mte=on \
+-global arm-smmuv3.secure-impl=on \
 -cpu max \
 -smp 1 \
 -m 8G \
