fc73c7998b633ed1751bd7ec8c9b6273a5863eb5
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-03-21 19:02:38 -0700

    container.sh: allow to run from anywhere

--- container.sh
+++ container.sh
@@ -2,14 +2,20 @@
 
 set -euo pipefail
 
+script_dir=$(dirname $(readlink -f $0))
+
 # build silently for 3 seconds, and restart if needed
-timeout 3 podman build -q -t build-linux-stack - < ./Dockerfile ||
-    podman build -t build-linux-stack - < ./Dockerfile
+timeout 3 podman build -q -t build-linux-stack - < $script_dir/Dockerfile ||
+    podman build -t build-linux-stack - < $script_dir/Dockerfile
 
 tty=-t
 [ -v CONTAINER_NO_TTY ] && tty=
 podman run \
-    -i $tty --rm -v $(pwd):$(pwd) -w $(pwd) -v $HOME:$HOME -e HOME=$HOME --init \
+    -i $tty --rm \
+    -v $script_dir:$script_dir \
+    -v $(pwd):$(pwd) \
+    -w $(pwd) -v $HOME:$HOME -e HOME=$HOME \
+    --init \
     --network host \
     --privileged \
     -e DISABLE_CONTAINER_CHECK=1 \
