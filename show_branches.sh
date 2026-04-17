7f0bcb78b67df35481a3852cbcbbea717bc9c4e4
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2026-04-17 15:05:18 -0700

    show_branches: create local branches from remote ones

--- show_branches.sh
+++ show_branches.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-set -x
 
+# generate all local branches for remote ones
+for branch in $(git branch -r | grep -v '\->'); do
+    git branch --track "${branch#origin/}" "$branch" >& /dev/null || true
+done
+
+set -x
 git log --all --decorate-refs='refs/heads/*' --oneline --graph --simplify-by-decoration
