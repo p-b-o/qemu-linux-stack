a97400663c363f61ca6407e674a70e83b837aef2
Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
2026-07-24 05:13:52 +0200

    macos: Bump max number of open files limit
    podman access the host filesystem over virtio-9p which is limited.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>

--- container.sh
+++ container.sh
@@ -7,7 +7,7 @@ script_dir=$(dirname $(readlink -f $0))
 context_hash=$(sha1sum $script_dir/Dockerfile | cut -f 1 -d ' ')
 image=build-linux-stack-$context_hash
 if ! podman image exists $image; then
-    podman build -t $image -f $script_dir/Dockerfile
+    podman build --ulimit=nofile=65536:65536 -t $image -f $script_dir/Dockerfile
 fi
 podman tag $image build-linux-stack
 
@@ -19,6 +19,7 @@ exec podman run \
     -v $(pwd):$(pwd) \
     -v /tmp:/tmp \
     -w $(pwd) -v $HOME:$HOME -e HOME=$HOME \
+    --ulimit=nofile=65536:65536 \
     --init \
     --network host \
     --privileged \
