eda3b19d8a9ec6149ff2e9a74023e2f40e33a96e
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-07-24 15:37:25 -0700

    fix: only SHM size and location should be set since the default was erasing existing fw-cfg

--- build_optee_os.sh
+++ build_optee_os.sh
@@ -25,8 +25,6 @@ build()
     PLATFORM=vexpress-qemu_armv8a \
     DEBUG=1 \
     CFG_ARM64_core=y \
-    CFG_EXTERNAL_DTB_OVERLAY=y \
-    CFG_DT_ADDR=0x40004000 \
     CFG_SHMEM_START=0x60800000 \
     CFG_SHMEM_SIZE=0x00200000 \
     CFG_USER_TA_TARGETS=ta_arm64 \
